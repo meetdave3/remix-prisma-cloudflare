f7c3f54783de1836a74432dec9718c83598e367c
Meet Dave <meet.dave@outlook.com>
2023-11-26 02:55:38 +0100

    chore: add tailwindcss

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/cloudflare";
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from "@prisma/client/edge";
 import { useLoaderData } from "@remix-run/react";
 
 import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
@@ -7,37 +7,39 @@ import type { Log } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 interface AccelerateInfo {
-  cacheStatus: 'ttl' | 'swr' | 'miss' | 'none'
-  lastModified: Date
-  region: string
-  requestId: string
-  signature: string
+  cacheStatus: "ttl" | "swr" | "miss" | "none";
+  lastModified: Date;
+  region: string;
+  requestId: string;
+  signature: string;
 }
 
 interface Env {
-  DATABASE_URL: string
-  DIRECT_URL: string
+  DATABASE_URL: string;
+  DIRECT_URL: string;
 }
 
 interface LoaderData {
-  logs: Log[]
-  info: AccelerateInfo | null
+  logs: Log[];
+  info: AccelerateInfo | null;
 }
 
 export async function loader(props: LoaderFunctionArgs) {
-  const env = props.context.env as Env
-  
+  const env = props.context.env as Env;
+
   const prisma = new PrismaClient({
     datasourceUrl: env.DATABASE_URL,
-  }).$extends(withAccelerate())
+  }).$extends(withAccelerate());
 
-  const logs = await prisma.log.findMany({
-    cacheStrategy: { ttl: 60 }
-  }).withAccelerateInfo();
+  const logs = await prisma.log
+    .findMany({
+      cacheStrategy: { ttl: 60 },
+    })
+    .withAccelerateInfo();
 
   return json<LoaderData>({
     logs: logs.data,
-    info: logs.info
+    info: logs.info,
   });
 }
 
@@ -45,30 +47,41 @@ export default function Index() {
   const props = useLoaderData<LoaderData>();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Database access on the Edge with Remix, Cloudflare workers & Prisma Data Proxy</h1>
+    <div className="max-w-5xl m-auto p-10">
+      <h1 className="text-3xl font-bold w-4/5">
+        Database access on the edge with Remix run, Cloudflare workers & Prisma
+        accelerate
+      </h1>
 
-      <h2>Query info (withAccelerate):</h2>
-      <ul>
-        Cache Status: 
-        {props.info?.cacheStatus}
-      </ul>
-      <ul>
-        Last modified: 
-        {props.info?.lastModified}
-      </ul>
-      <ul>
-        Region: 
-        {props.info?.region}
-      </ul>
-      <ul>
-        RequestId: 
-        {props.info?.requestId}
-      </ul>
-      <ul>
-        Signature: 
-        {props.info?.signature}
-      </ul>
+      <div className="pt-6">
+        <h2 className="my-2 text-2xl">Query info:</h2>
+        <div className="flex flex-col gap-2">
+          <ul>
+            Cache Status:
+            {props.info?.cacheStatus}
+          </ul>
+          <ul>
+            Last modified:
+            {props.info?.lastModified}
+          </ul>
+          <ul>
+            Region:
+            {props.info?.region}
+          </ul>
+          <ul>
+            RequestId:
+            {props.info?.requestId}
+          </ul>
+          <ul>
+            Signature:
+            {props.info?.signature}
+          </ul>
+          <ul>
+            Payload:
+            {JSON.stringify(props.logs, null, 2)}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
